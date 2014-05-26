00cb901007e15aab9c2e8ec2cfa2ed8334d6d427
Evgenii Nikolaev <evgeny.albertovich@gmail.com>
2014-05-26 21:09:05 +0400

    added work with input file

--- main.c
+++ main.c
@@ -23,23 +23,31 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-/*
-	TODO:
-    if (argc == 4) {
-        read file
-    }
-*/     
     size_t nmemb=atoi(argv[1]), size=atoi(argv[2]);
     int *arr = (int*)malloc(nmemb*size);
     size_t i, n=nmemb*size/sizeof(int);
-    for (i=0; i<n; i++)
-        arr[i] = rand() % nmemb;
 
-    printf("BEFORE sort:\n");
+    if (argc == 4) {
+        FILE *fp = fopen(argv[3], "r");
+        for (i=0; i<n; i++) {
+            fscanf(fp, "%d", &arr[i]);
+        }
+        fclose(fp);
+    }
+    else {
+        for (i=0; i<n; i++)
+            arr[i] = rand() % nmemb;
+    }
+
+    printf("BEFORE:\n");
     print_array(arr, n);
+    printf("\n");
+
     sort((void *)arr, nmemb, size, cmp_int);
 
-    printf("\nAFTER sort:\n");
+    printf("AFTER:\n");
     print_array(arr, n);
+    printf("\n");
+
     return 0;
 }
