8980781b73094290b78d954584bdfbfa4a4a577a
Evgenii Nikolaev <evgeny.albertovich@gmail.com>
2014-05-26 21:08:52 +0400

    changed way of printing

--- gen_data.py
+++ gen_data.py
@@ -6,6 +6,6 @@ MAX = 100500
 if __name__ == '__main__':
     f = open(FILENAME, 'w')
     for i in range(MAX):
-        f.write(str(random.randint(0,MAX))) 
+        f.write( str(random.randint(0,MAX)) + ' ' ) 
 
     f.close()
